1c00314a35c15f37239e5a84957c6d75b12a41c9
Matheus de Alcantara <162853962+malcantara-dev@users.noreply.github.com>
2025-02-13 15:21:09 +0000

    adicionado scripts `lint:check` e `link:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Meu primeiro projeto para estudo geral
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Tudo funcionando!</h1>
+  return <h1>Tudo funcionando!</h1>;
 }
 
 export default Home;
