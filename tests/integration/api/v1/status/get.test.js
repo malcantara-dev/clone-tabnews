33c7f50aa21c4623e7f22dcc00bea90a5cda8b2d
Matheus de Alcantara <162853962+malcantara-dev@users.noreply.github.com>
2025-02-27 17:59:11 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
+const { Client, Query } = require("pg");
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(parseInt(responseBody.dependencies.database.opened_connections)).toBe(
+    1,
+  );
+
+  /*
+  versao do postgres
+  conexoes maximas
+  conexoes usadas
+  */
 });
