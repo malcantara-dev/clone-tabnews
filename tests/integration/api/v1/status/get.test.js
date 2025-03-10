5a3ffa2b01af8e60d20e1fdc77cfbfa2da30a973
Matheus de Alcantara <162853962+malcantara-dev@users.noreply.github.com>
2025-03-10 14:01:44 +0000

    added ssl connection to production database

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,15 +10,9 @@ test("GET to /api/v1/status should return 200", async () => {
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.version).toEqual("16.8");
+  expect(responseBody.dependencies.database.max_connections).toBe(901);
   expect(parseInt(responseBody.dependencies.database.opened_connections)).toBe(
     1,
   );
-
-  /*
-  versao do postgres
-  conexoes maximas
-  conexoes usadas
-  */
 });
